9edef90db651d8616e547b3892a496649549d07e
Bhuv27nesh <27bhuvanesh5@gmail.com>
2025-07-30 11:14:24 +0530

    Update alu_generator.sv

--- src/TOP/Test_Block/alu_generator.sv
+++ src/TOP/Test_Block/alu_generator.sv
@@ -1,32 +1,34 @@
 `include "alu_defines.sv"
+
 class alu_generator;
 //PROPERTIES
-//ALU transaction class handle
- alu_transaction blueprint;
+//ALU transaction class handle 
+  alu_transaction blueprint;
 
 //Mailbox for generator to driver connection
-  mailbox #(alu_transaction)mbx_gd;
+  mailbox #(alu_transaction) mbx_gd;
 
 //METHODS
 //Explicitly overriding the constructor to make mailbox
 //connection from generator to driver
- function new(mailbox #(alu_transaction)mbx_gd);
+  function new(mailbox #(alu_transaction) mbx_gd);
     this.mbx_gd = mbx_gd;
     blueprint = new();
- endfunction
+  endfunction
 
 //Task to generate the random stimuli
- task start();
-  for(int i=0;i<`num_transactions;i++)
-   begin
-   //Randomizing the inputs
-   void'(blueprint.randomize());
+  task start();
+    for(int i = 0; i < `num_transaction; i++) 
+    begin
+      //Randomizing the inputs
+      void'(blueprint.randomize());
 
-   //Putting the randomized inputs to mailbox
-   mbx_gd.put(blueprint.copy());
-     #1;
-     $display("%0t GENERATOR Randomized transaction: OPA = %0d | OPB = %0d | CIN = %0d | CE = %0d | MODE = %0d | INP_VALID = %0d | CMD = %0d",$time,blueprint.OPA,blueprint.OPB, blueprint.CIN, blueprint.CE,blueprint.MODE,blueprint.INP_VALID,blueprint.CMD);
-   end
- endtask
+      //Putting the randomized inputs to mailbox 
+      mbx_gd.put(blueprint.copy());
+     $display("%0t GENERATOR Randomized transaction: OPA = %0d | OPB = %0d | CIN = %0d | MODE = %0d | INP_VALID = %0d | CMD = %0d",$time,blueprint.OPA,blueprint.OPB, blueprint.CIN, blueprint.MODE,blueprint.INP_VALID,blueprint.CMD);
+      $display("-------------------------GENERATOR--------------------------------");
+    end
+  endtask
+
 
 endclass

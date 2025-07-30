2b2de1170230ac1f2961c9aa1b9b55aef06d58ad
Bhuv27nesh <27bhuvanesh5@gmail.com>
2025-07-30 11:18:19 +0530

    Update alu_package.sv

--- src/TOP/alu_package.sv
+++ src/TOP/alu_package.sv
@@ -1,11 +1,10 @@
-package alu_package;
-
-`include "alu_transaction.sv"
-`include "alu_generator.sv"
-`include "alu_driver.sv"
-`include "alu_monitor.sv"
-`include "alu_reference_model.sv"
-`include "alu_scoreboard.sv"
-`include "alu_environment.sv"
-`include "alu_test.sv"
+package alu_pkg;  
+  `include "alu_transaction.sv"
+  `include "alu_generator.sv"
+  `include "alu_driver.sv"
+  `include "alu_reference_model.sv"
+  `include "alu_monitor.sv"
+  `include "alu_scoreboard.sv"
+  `include "alu_environment.sv"
+  `include "alu_test.sv" 
 endpackage

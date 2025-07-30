2a574be622cc3d8dc33cc882b78e424696584943
Bhuv27nesh <27bhuvanesh5@gmail.com>
2025-07-30 11:14:55 +0530

    Update alu_monitor.sv

--- src/TOP/Test_Block/alu_monitor.sv
+++ src/TOP/Test_Block/alu_monitor.sv
@@ -1,56 +1,118 @@
 `include "alu_defines.sv"
 
 class alu_monitor;
- //ALU transaction class handle  
-  alu_transaction mon_tran;
- //mailbox of monitor to scoreboard 
+ //ALU transaction class handle
+  alu_transaction mon_trans;
+  // mailbox for monitor to scoreboard
   mailbox #(alu_transaction) mbx_ms;
-  //Virtual interfaces for  monitor
-  virtual alu_if.MON vif;
-  
-  // Coverage model to cover DUV outputs
-    covergroup mon_cov;
-      ERROR : coverpoint mon_tran.ERR {bins error ={0,1};}
-      RESULT: coverpoint mon_tran.RES {bins result = {[0:2**`DATA_WIDTH]};}
-      OF_FLOW: coverpoint mon_tran.OFLOW{bins oflow={0,1};}
-      GREATER: coverpoint mon_tran.G {bins greater={0,1};}
-      LESSER:  coverpoint mon_tran.L {bins lesser={0,1};}
-      EQUAL :  coverpoint mon_tran.E {bins equal={0,1};}
-      RESxOF : cross RESULT,OF_FLOW;
-    endgroup
-  // alu_monitor class constructor
-  function new( virtual alu_if.MON vif, mailbox #(alu_transaction) mbx_ms);
-    this.vif=vif;
-    this.mbx_ms=mbx_ms; 
-    mon_cov=new();
+ //Virtual interfaces for  monitor
+  virtual alu_if.MON mon_vif;
+
+   covergroup mon_cg;
+      RESULT  : coverpoint mon_trans.RES {bins result = {[0:9'b111111111]};}
+      ERROR   : coverpoint mon_trans.ERR;
+      EQUAL   : coverpoint mon_trans.E {bins equal   = {1};}
+      GREATER : coverpoint mon_trans.G {bins greater = {1};}
+      LESSER  : coverpoint mon_trans.L {bins lesser  = {1};}
+      OF_LOW  : coverpoint mon_trans.OFLOW;
+      COUT_CP : coverpoint mon_trans.COUT;
+  endgroup
+ 
+ // alu_monitor class constructor
+  function new(virtual alu_if.MON mon_vif, mailbox #(alu_transaction) mbx_ms);
+    this.mbx_ms = mbx_ms;
+    this.mon_vif = mon_vif;
+    mon_cg = new();
   endfunction
-  
-  
-  task start();
-    repeat(6) @(vif.mon_cb); 
-    for(int i=0;i<`num_transactions;i++)
-      begin
-        mon_tran=new();
-        repeat(1) @(vif.mon_cb)
-          begin
-            mon_tran.ERR=vif.mon_cb.ERR;
-            mon_tran.RES=vif.mon_cb.RES;
-            mon_tran.OFLOW=vif.mon_cb.OFLOW;
-            mon_tran.COUT=vif.mon_cb.COUT;
-            mon_tran.G=vif.mon_cb.G;
-            mon_tran.L=vif.mon_cb.L;
-            mon_tran.E=vif.mon_cb.E;
-          end
-        $display("%0t | MONITOR PASSING THE DATA TO SCOREBOARD \nERR=%0d | RES=%0d | OFLOW=%0d | COUT=%0d | G=%0d | L=%0d | E=%0d ", $time, mon_tran.ERR,mon_tran.RES,mon_tran.OFLOW,mon_tran.COUT,mon_tran.G,mon_tran.L,mon_tran.E);
-        
-        //Putting the collected outputs to mailbox 
-        mbx_ms.put(mon_tran);
-        
-        //Sampling the covergroup
-        mon_cov.sample();
-        //$display("\n OUTPUT FUNCTIONAL COVERAGE = %0d", mon_cov.get_coverage());
-        repeat(1) @(vif.mon_cb);
-      end
+
+  task signal_drive();
+    mon_trans.RES   = mon_vif.mon_cb.RES;
+    mon_trans.COUT  = mon_vif.mon_cb.COUT;
+    mon_trans.OFLOW = mon_vif.mon_cb.OFLOW;
+    mon_trans.G     = mon_vif.mon_cb.G;
+    mon_trans.L     = mon_vif.mon_cb.L;
+    mon_trans.E     = mon_vif.mon_cb.E;
+    mon_trans.ERR   = mon_vif.mon_cb.ERR;
+    //inputs
+    mon_trans.OPA = mon_vif.mon_cb.OPA;
+    mon_trans.OPB = mon_vif.mon_cb.OPB;
+    mon_trans.CIN = mon_vif.mon_cb.CIN;
+    mon_trans.CMD = mon_vif.mon_cb.CMD;
+    mon_trans.MODE = mon_vif.mon_cb.MODE;
+    mon_trans.INP_VALID = mon_vif.mon_cb.INP_VALID;
+    mon_cg.sample();
   endtask
-  
+
+  task start();
+    int single_op_arithmetic [] = {4,5,6,7};
+    int single_op_logical [] = {6,7,8,9,10,11};
+    int count = 0;
+    
+    repeat(4)@(mon_vif.mon_cb);
+    for(int i = 0; i < `num_transaction; i++) begin 
+      mon_trans = new();
+      signal_drive();
+
+      if((mon_trans.INP_VALID == 2'b11) || mon_trans.INP_VALID == 2'b00) begin 
+        signal_drive();
+        if((mon_trans.MODE == 1)&& ((mon_trans.CMD == 4'b1010) || (mon_trans.CMD == 4'b1001))) begin 
+          repeat(2)@(mon_vif.mon_cb);
+          signal_drive();
+          $display("%0t | MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID, mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+          $display("DUT OUTPUT RECIEVED : RES == %0d | ERR = %0d",mon_trans.RES,mon_trans.ERR);
+        end 
+        else begin 
+          repeat(1)@(mon_vif.mon_cb);
+          signal_drive();
+          $display("%0t | MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+          $display("DUT OUTPUT RECIEVED : RES == %0d | ERR = %0d",mon_trans.RES,mon_trans.ERR);
+        end 
+      end 
+
+      else begin 
+        if((mon_trans.MODE == 0) && (mon_trans.CMD inside {single_op_logical})) begin 
+          repeat(1)@(mon_vif.mon_cb);
+          signal_drive();
+          $display("%0t |MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);                    
+        end 
+        else if ((mon_trans.MODE == 1) && mon_trans.CMD inside {single_op_arithmetic}) begin 
+          repeat(1)@(mon_vif.mon_cb);
+          signal_drive();
+          $display("%0t | MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+        end 
+        else begin 
+          for(count = 0; count < 16; count ++) begin 
+            repeat(1) @(mon_vif.mon_cb);
+            signal_drive();
+            if(mon_trans.INP_VALID == 2'b11) begin 
+              if((mon_trans.MODE == 1)&& ((mon_trans.CMD == 9) || (mon_trans.CMD == 10))) begin
+                signal_drive();
+                $display("%0t | MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+                $display("%0t | DUT OUTPUT RECEIVED : RES == %0d | ERR = %0d",$time,mon_trans.RES,mon_trans.ERR);
+              end
+                else begin
+                  signal_drive();
+                  $display("%0t | MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+                  $display("%0t | DUT OUTPUT RECEIVED : RES == %0d | ERR = %0d",$time,mon_trans.RES,mon_trans.ERR);
+                end 
+              break;
+            end 
+          end 
+          if(count == 16) begin 
+            signal_drive();
+            $display("%0t | !!!ERROR!!! |MONITOR | MODE = %0b | INP_VALID = %2b | CMD = %4b | OPA = %3d | OPB = %3d | CIN = %0d ",$time,mon_trans.MODE,mon_trans.INP_VALID,mon_trans.CMD, mon_trans.OPA,mon_trans.OPB,mon_trans.CIN);
+            $display("DUT OUTPUT : RES =  %0d | ERR = %0d", mon_trans.RES,mon_trans.ERR);
+          end 
+ 
+        end 
+      end 
+      if((mon_trans.MODE == 1) && ((mon_trans.CMD == 4'b1001) || (mon_trans.CMD == 4'b1010))) begin
+        repeat(1)@(mon_vif.mon_cb);
+      end
+      repeat(1) @(mon_vif.mon_cb);
+      signal_drive();
+      mbx_ms.put(mon_trans);
+    end 
+  endtask
+
 endclass
